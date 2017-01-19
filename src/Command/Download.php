10c4ebaf1374777fc38f87a320115e78288415c9
wangxuan <wangxuan@qgpic.com>
2017-01-19 11:09:55 +0800

    refactoring

--- src/Command/Download.php
+++ src/Command/Download.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace Fuguevit\NHDownloader;
+namespace Fuguevit\NHDownloader\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class Downloader extends Command
+class Download extends Command
 {
     public function configure()
     {
