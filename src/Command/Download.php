312ee6b5207366451abd209799a355c15e0671e6
wangxuan <wangxuan@qgpic.com>
2017-01-24 14:45:42 +0800

    refactor zip archive

--- src/Command/Download.php
+++ src/Command/Download.php
@@ -4,7 +4,7 @@ namespace Fuguevit\NHDownloader\Command;
 
 use Fuguevit\NHDownloader\Downloader;
 use Fuguevit\NHDownloader\Exception\GuzzleResultCodeError;
-use Fuguevit\NHDownloader\Helper\NHZipArchiver;
+use Fuguevit\NHDownloader\Helper\NHZipArchive;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -25,21 +25,32 @@ class Download extends Command
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $id = $input->getArgument('id');
+        
+        $this->startDownload($id, $input, $output)
+            ->checkCompress($input->getOption('archive'), $id);
+
+        $output->writeln('Download Success!');
+    }
+    
+    protected function startDownload($id, InputInterface $input, OutputInterface $output)
+    {
         try {
             $downloader = new Downloader($id, $input->getOption('proxy'));
             $downloader->start();
         } catch (GuzzleResultCodeError $exception) {
             $output->writeln($exception->getMessage());
         }
+        
+        return $this;
+    }
+    
+    protected function checkCompress($flag, $id)
+    {
+        if (!$flag) return;
 
-        if ($input->getOption('archive')) {
-            $zipFileName = __DIR__.'/../../storage/'.$id.'.zip';
-            $za = new NHZipArchiver();
-            $za->open($zipFileName, \ZipArchive::CREATE);
-            $za->addDir(__DIR__.'/../../storage/'.$id, $id);
-            $za->close();
-        }
-
-        $output->writeln('Download Success!');
+        $zipFile = __DIR__.'/../../storage/'.$id.'.zip';
+        $dirLocation = __DIR__.'/../../storage/'.$id;
+        
+        NHZipArchive::zipFolder($dirLocation, $zipFile);
     }
 }
---
+++ src/Helper/NHZipArchive.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Fuguevit\NHDownloader\Helper;
+
+use ZipArchive;
+
+class NHZipArchive extends ZipArchive
+{
+    /**
+     * @param $location
+     * @param $zipName
+     * @param $removeOriginal
+     * 
+     * @return mixed
+     */
+    public static function zipFolder($location, $zipName, $removeOriginal = true)
+    {
+        $newDirName = end(explode(DIRECTORY_SEPARATOR, rtrim($location, DIRECTORY_SEPARATOR)));
+        
+        $zip = new static;
+        $zip->open($zipName, ZipArchive::CREATE);
+        $zip->addDir($location, $newDirName);
+        $zip->close();
+
+        if ($removeOriginal) {
+            $zip->removeDir($location);
+        }
+    }
+
+    public function addDir($location, $name)
+    {
+        $this->addEmptyDir($name);
+        $this->addDirDo($location, $name);
+
+    }
+
+    protected function addDirDo($location, $name)
+    {
+        $name .= '/';
+        $location .= '/';
+        $dir = opendir($location);
+
+        while ($file = readdir($dir)) {
+            if ($file == '.' || $file == '..') {
+                continue;
+            }
+            $do = (filetype($location.$file) == 'dir') ? 'addDir' : 'addFile';
+            $this->$do($location.$file, $name.$file);
+        }
+    }
+
+    protected function removeDir($dir) {
+        if (is_dir($dir)) {
+            $objects = scandir($dir);
+            foreach ($objects as $object) {
+                if ($object != "." && $object != "..") {
+                    if (filetype($dir."/".$object) == "dir") rrmdir($dir."/".$object); else unlink($dir."/".$object);
+                }
+            }
+            reset($objects);
+            rmdir($dir);
+        }
+    }
+}
